69182897f9f9276e40b3aa33bbcbe6f3b5fe4874
App Academy Student <email@email.com>
2019-12-31 11:17:40 -0500

    Add more stylings, logout button, and modal fixes

--- frontend/src/components/home/nav/nav.jsx
+++ frontend/src/components/home/nav/nav.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { logout } from '../../../actions/session_actions';
+import { openModal } from '../../../actions/modal_actions';
 
 class Nav extends React.Component {
 
   render() {
+    const { logout, openModal } = this.props;
+
     return(
       <nav className='nav-container'>
-        <header>
-          Nav header
+        <header className='nav-header'> 
+          <div>UpNext</div>
+          <button
+            className='logout-button'
+            onClick={logout}>
+            Logout
+          </button>
         </header>
 
-        <div>
-          Search component
+        <div className='nav-search'>
+          <input
+            type="text"
+            placeholder="Search..."
+            onClick={() => openModal('tester')}/>
         </div>
       </nav>
     );
   }
 }
 
-export default connect(null, null)(Nav);
+const mdp = dispatch => ({
+  logout: () => dispatch(logout()),
+  openModal: modal => dispatch(openModal(modal))
+});
+
+export default connect(null, mdp)(Nav);

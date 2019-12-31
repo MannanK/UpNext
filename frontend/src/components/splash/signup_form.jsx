1e36384ef3d079d84cfd7d60eb37168e13913663
App Academy Student <email@email.com>
2019-12-31 11:56:59 -0500

    finish splash css

--- frontend/src/components/splash/signup_form.jsx
+++ frontend/src/components/splash/signup_form.jsx
@@ -5,13 +5,15 @@ class SignupForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      username: '',
-      password: '',
-      password2: ''
+      email: "",
+      username: "",
+      password: "",
+      password2: ""
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderErrorHighlight = this.renderErrorHighlight.bind(this);
+
   }
 
   // componentDidUpdate(prevProps){
@@ -27,9 +29,10 @@ class SignupForm extends React.Component {
   }
 
   update(field) {
-    return e => this.setState({
-      [field]: e.currentTarget.value
-    });
+    return e =>
+      this.setState({
+        [field]: e.currentTarget.value
+      });
   }
 
   handleSubmit(e) {
@@ -41,20 +44,11 @@ class SignupForm extends React.Component {
       password2: this.state.password2
     };
 
-    this.props.signup(user, this.props.history); 
+    this.props.signup(user, this.props.history);
   }
 
-  renderErrors() {
-    return(
-
-      <ul className="session-errors">
-        {Object.keys(this.props.errors).map((error, i) => (
-          <li key={`${i}`}>
-            {this.props.errors[error]}
-          </li>
-        ))}
-      </ul>
-    );
+  renderErrorHighlight(type) {
+    return this.props.errors[type] ? "error" : "";
   }
 
   render() {
@@ -65,35 +59,42 @@ class SignupForm extends React.Component {
 
           <div className="signup-form">
             <input
+              className={this.renderErrorHighlight("username")}
               type="text"
               value={this.state.username}
               onChange={this.update("username")}
               placeholder="Username"
             />
+            <div className="error-caption">{this.props.errors.username}</div>
 
             <input
+              className={this.renderErrorHighlight("email")}
               type="text"
               value={this.state.email}
               onChange={this.update("email")}
               placeholder="Email"
             />
+            <div className="error-caption">{this.props.errors.email}</div>
 
             <input
+              className={this.renderErrorHighlight("password")}
               type="password"
               value={this.state.password}
               onChange={this.update("password")}
               placeholder="Password"
             />
+            <div className="error-caption">{this.props.errors.password}</div>
 
             <input
+              className={this.renderErrorHighlight("password2")}
               type="password"
               value={this.state.password2}
               onChange={this.update("password2")}
               placeholder="Confirm Password"
             />
+            <div className="error-caption">{this.props.errors.password2}</div>
 
             <input className="form-submit" type="submit" value="Sign Up" />
-            {this.renderErrors()}
           </div>
         </form>
       </>

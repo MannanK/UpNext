3987aaa0975ee20e0210c8a303784599ef2418f2
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-01-01 12:45:53 -0500

    Make second omdb API call to get full info of title user clicks on, add lodash.isEmpty, fix demo login breaking the url

--- frontend/src/components/modal/search.jsx
+++ frontend/src/components/modal/search.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { omdbApiKey } from '../../config/keys';
-// import debounce from "lodash.debounce";
+
 const debounce = require("lodash.debounce");
+const isEmpty = require("lodash.isempty");
 
 class Search extends React.Component {
   constructor(props) {
@@ -14,7 +15,6 @@ class Search extends React.Component {
       searchResults: []
     };
 
-    // this.handleInput = this.handleInput.bind(this);
     this.handleInput = this.handleInput.bind(this);
     this.makeDebouncedSearch = debounce(this.makeDebouncedSearch, 700);
     this.handleClick = this.handleClick.bind(this);
@@ -24,13 +24,12 @@ class Search extends React.Component {
     let keyword = e.currentTarget.value;
 
     this.setState({keyword});
-    console.log(this.state.keyword);
 
     // 1. make the omdb call to get the final interest the user chose
     if (keyword !== "") {
       this.makeDebouncedSearch(keyword);
     }
-  };
+  }
 
   makeDebouncedSearch(keyword) {
     const instance = axios.create();
@@ -38,10 +37,11 @@ class Search extends React.Component {
     instance.defaults.headers.common.accept = 'application/json';
 
     instance
-      .get(`http://www.omdbapi.com/?s=${keyword}&apikey=${omdbApiKey}`)
+      .get(`http://www.omdbapi.com/?s=${keyword}&type=movie&apikey=${omdbApiKey}`)
       .then(response => {
-        let searchResults = response.data["Search"];
-        if (searchResults !== null) {
+        let searchResults = response.data.Search;
+
+        if (!isEmpty(searchResults)) {
           searchResults = searchResults.slice(0, 9);
         }
 
@@ -54,17 +54,30 @@ class Search extends React.Component {
   // first make a call to omdb to get the full interest info
   // then make a call to our own backend with the return value of that, to add
     // a new interest
-  handleClick(e) {
-    
-    
+  handleClick(title) {
+    return e => {
+      e.preventDefault();
+
+      const instance = axios.create();
+      instance.defaults.headers.common = {};
+      instance.defaults.headers.common.accept = 'application/json';
+
+      instance
+        .get(`http://www.omdbapi.com/?t=${title}&apikey=${omdbApiKey}`)
+        .then(response => {
+          console.log(response.data);
+        });
+    };
   }
 
   render() {
-    console.log(this.state.searchResults);
+    const { searchResults } = this.state;
 
-    let results = this.state.searchResults.map((result, idx) => {
-      return <li onClick={this.handleClick} key={idx}> {result.Title} </li>
-    });
+    let results = !isEmpty(searchResults) ? (
+      searchResults.map((result, idx) => {
+        return <li onClick={this.handleClick(result.Title)} key={idx}>{result.Title}</li>
+      })
+    ) : "";
 
     return(
       <div className='search-container'>

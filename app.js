470de2f864b46b20375b08160e6e51b9bfd81888
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-01-02 14:24:17 -0500

    Add required files and changes to deploy to Heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const recommendations = require("./routes/api/recommendations");
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const app = express();
+const path = require('path');
 
 const mongoose = require('mongoose');
 
@@ -12,6 +13,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 require('./config/passport')(passport);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 app.use("/api/users", users);
 app.use("/api/interests", interests);

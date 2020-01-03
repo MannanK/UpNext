d57a943455acff8fecf1e8e164b0373ed008ea34
App Academy Student <email@email.com>
2020-01-03 08:40:40 -0500

    Add recommendations stylings

--- frontend/src/components/home/recommendations/recommendations.jsx
+++ frontend/src/components/home/recommendations/recommendations.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
+// temp
+import { fetchInterests } from '../../../actions/interest_actions';
+// import { getRecommendations } from '../../../actions/recommendation_actions';
+import SimpleSlider from '../slider/simple_slider';
 
 class Recommendations extends React.Component {
+  componentDidMount() {
+    this.props.fetchInterests();
+  }
 
   render() {
     return (
       <div className='recommendations-container'>
-        These are your recs
+        <section className='recommendations-direct'>
+          <header className='slider-header'>Uniquely yours</header>
+          <SimpleSlider items={this.props.interests} />
+        </section>
+        <section className='recommendations-direct'>
+          <header className='slider-header'>Try something new, but not too new</header>
+          <SimpleSlider items={this.props.interests} />
+        </section>
+
       </div>
     );
   }
 }
 
-export default connect(null, null)(Recommendations);
+const msp = state => ({
+  interests: state.entities.interests
+});
+
+const mdp = dispatch => ({
+  fetchInterests: () => dispatch(fetchInterests())
+});
+
+export default connect(msp, mdp)(Recommendations);

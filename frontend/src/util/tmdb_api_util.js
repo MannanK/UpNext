198683368570255e17fc2cfb972eb57a3768b2e2
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-01-22 17:22:39 -0500

    Add util function for discover_movies API call

--- frontend/src/util/tmdb_api_util.js
+++ frontend/src/util/tmdb_api_util.js
@@ -16,6 +16,22 @@ export const getSimilarRecommendations = function(id) {
   return instance.get(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${tmdbApiKey}`);
 };
 
+export const getAllRecommendations = function (genreIds, page = 1) {
+  // put API docs URL
+  // test out include_video
+
+  let release_date = new Date(Date.now()).toISOString().split("T")[0];
+  let genres = genreIds.join("%2C");
+
+  return instance
+    .get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${tmdbApiKey}` +
+      `&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false` + 
+      `&page=${page}&release_date.lte=${release_date}&vote_count.gte=100&vote_average.gte=5` + 
+      `&with_genres=${genres}`
+    );
+};
+
 export const getMovieSuggestions = function(keyword) {
   // https://developers.themoviedb.org/3/search/search-movies
 
